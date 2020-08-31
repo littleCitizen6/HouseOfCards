ae78da51679f0441ce84afd0c84d96b4f7650784
citizen6 <theLittleCitizen6@gmail.com>
2020-08-31 10:13:59 +0300

    added id to participent

--- HouseOfCards/HouseOfCards/Cards/CardOnHands.cs
+++ HouseOfCards/HouseOfCards/Cards/CardOnHands.cs
@@ -9,6 +9,12 @@ namespace HouseOfCards.Cards
     {
         public Card Card { get; set; }
         public Participant Participant { get; set; }
-        public bool Hinted { get; set; }
+        public bool IsHinted { get; set; }
+        public CardOnHands(Card card, Participant participant)
+        {
+            Card = card;
+            Participant = participant;
+            IsHinted = false;
+        }
     }
 }
