604c519a47b501ffcfdfdf8f236bd6f28663b687
citizen6 <theLittleCitizen6@gmail.com>
2020-08-31 18:11:18 +0300

    added throwcard operation

--- HouseOfCards/HouseOfCards/Operations/ShowCardsOption.cs
+++ HouseOfCards/HouseOfCards/Operations/ShowCardsOption.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using HouseOfCards.Cards;
+using HouseOfCards.Participents;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HouseOfCards.Operations
 {
     public class ShowCardsOption : IOperation
     {
-        public void Operate()
+        public void Operate(Participant participant, GameManeger maneger)
         {
-            throw new NotImplementedException();
+            var cards = maneger.Info.CardOnHands.Where(x => x.Participant.Id == participant.Id && x.IsHinted);
+            if (cards != null)
+            {
+                List<Card> optional = new List<Card>();
+                maneger.Info.ExposedCard.Keys.ToList().ForEach(color => optional.Add(maneger.Info.ExposedCard[color].Last()));
+            }
         }
     }
 }
